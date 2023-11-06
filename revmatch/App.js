ad0a26daea298393cd069c91cd297902c23a66ae
Graehm <gpatrellfazio@gmail.com>
2023-11-05 21:42:26 -0600

    more dependencies but tw is breaking the app its reading it as a function

--- revmatch/App.js
+++ revmatch/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+// import {useTailwind} from 'tailwind-rn';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello from the rev match app</Text>
       <StatusBar style="auto" />
     </View>
   );
